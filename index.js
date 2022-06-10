80378d45974316b31158c3c83de582f4d6c6a83c
syphrose <syphrose.wabulwenyi@student.moringaschool.com>
2022-06-11 01:12:53 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (num1,num2) =>{
+    return 2000/100
+    
+}
+// divide(2000/100)=>20 || =>NaN
+
+const add = (parameter1, parameter2) =>parameter1+parameter2;
+
+const square = (c) =>{
+    return c*c;
+}
+
